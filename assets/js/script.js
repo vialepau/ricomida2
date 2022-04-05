1a8d4c989b7b552451c3b8bac6223fb69716c9f2
Paulina Viale <vialepau@gmail.com>
2022-04-05 02:53:46 -0400

    diseño listo...

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    $('[data-toggle="popover"]').popover({
+        container: 'body'
+    })
+  })
+
+
+
+/*$(document).ready(function(){
+
+    $('.btnUno').popover({content: "Blabla", trigger: "hover"}); 
+    $('.btnDos').popover({content: "Blabla", trigger: "hover"}); 
+    
+  });
+  $(".btnUno").on(function(){
+    $("[data-toggle='popover']").popover('toggle');
+  });*/
